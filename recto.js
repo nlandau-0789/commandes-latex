5ab75c261e0b0df2a3b635d9cfa10ccfda35922e
Marc-assin <marc.descamps.1666@gmail.com>
2026-01-22 18:32:38 +0100

    Floor.js

--- recto.js
+++ recto.js
@@ -36,6 +36,7 @@ MathJax.config.tex['macros'] = {
 		abs: ["\\left| #1 \\right|", 1],
 		norm: ["\\left|\\left| #1 \\right|\\right|", 1],
 		opnorm: ["\\left|\\left|\\left| #1 \\right|\\right|\\right|", 1],
+		floor: ["\\lfloor #1 \\rfloor", 1],
     	Sp: "\\operatorname{Sp}",
     	Rac: "\\operatorname{Rac}",
     	Tr: "\\operatorname{Tr}",
